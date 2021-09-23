f5cb3ef240286e3b7ecf41207ecd1cf836c1c3bd
Roy Avidar <Roi981av@gmail.com>
2021-09-23 16:37:57 +0300

    cart migration added

---
+++ src/migration/1632404258237-cart.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class cart1632404258237 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
