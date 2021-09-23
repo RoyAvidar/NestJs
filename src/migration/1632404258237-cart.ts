5841f38626277bba6548d906e31e47e4d2513f93
Roy Avidar <Roi981av@gmail.com>
2021-09-23 16:43:44 +0300

    few changes in cart migration

--- src/migration/1632404258237-cart.ts
+++ src/migration/1632404258237-cart.ts
@@ -1,11 +1,25 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class cart1632404258237 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: "cart",
+            columns: [
+                {
+                    name: "cartId",
+                    type: "bigInt",
+                    isGenerated: true,
+                    generationStrategy: "increment",
+                    isPrimary: true,
+                    unsigned: true
+                },
+            ],
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("cart");
     }
 
 }
