db2125aa103d171f4fb98965f10f2a103d03d02e
Roy Avidar <Roi981av@gmail.com>
2021-11-28 13:46:40 +0200

    commit

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,8 +1,9 @@
 import { Field, ObjectType } from "@nestjs/graphql";
 import { Order } from "src/entity/order.entity";
 import { Product } from "src/entity/product.entity";
-import { BaseEntity, Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, BeforeInsert, Column, Entity, JoinColumn, JoinTable, ManyToMany, OneToMany, OneToOne, PrimaryGeneratedColumn } from "typeorm";
 import { Cart } from "./cart.entity";
+import * as bcrypt from 'bcrypt';
 
 @Entity('users')
 @ObjectType()
@@ -42,4 +43,10 @@ export class User extends BaseEntity{
     @OneToOne(type => Cart, Cart => Cart.user)
     @Field(type => Cart)
     cart?: Cart;
+
+    @BeforeInsert()
+    async hashPassword() {
+        const saltOrrounds = 10;
+        this.userPassword = await bcrypt.hash(this.userPassword, saltOrrounds);
+    }
 }
