743c34e8e5a9b5b4df8b727b0bfb07577b52e574
Roy Avidar <Roi981av@gmail.com>
2021-12-05 16:35:47 +0200

    small changes

--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -4,6 +4,7 @@ import { User } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
 import { GraphQLUpload, FileUpload } from 'graphql-upload';
 import { createWriteStream } from 'fs';
+import {v4 as uuidv4} from 'uuid';
 
 @Injectable()
 export class PhotosService {
@@ -15,9 +16,12 @@ export class PhotosService {
     async uploadFile(user: User, {createReadStream, filename}: FileUpload): Promise<boolean> {
         const realUser = await this.userRepository.findOneOrFail(user.userId);
         // get the fileName and split it so we have only the .png/.jpg
+        const newFileName = filename.split(".");
+        console.log(newFileName);
         // give the fileName a uuid ID so that we have a 00001.png etc
-        // realUser.userProfilePic = filename; 
+        uuidv4();
         // updateUser = await this.userRepository.update(realUser); 
+        // this.userRepository.update(realUser.userProfilePic, filename);
         return new Promise(async (resolve, reject) => 
             createReadStream()
                 .pipe(createWriteStream(`./uploads/${filename}`)) //the new file name with the uuid.png..
