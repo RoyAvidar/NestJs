875263b237fc51b75dd8ab2613f7ad350e569f41
Damrir <roi981av@gmail.com>
2021-12-18 15:32:55 +0200

    small changes

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe()); //global pipe.
-  app.useStaticAssets(join(__dirname, '..', 'uploads'));
+  app.useStaticAssets(join(__dirname, '..', 'user-uploads'));
   app.useStaticAssets(join(__dirname, '..', 'product-uploads'));
   app.use(graphqlUploadExpress()); //global pipe for uploading files.
   await app.listen(8000);
